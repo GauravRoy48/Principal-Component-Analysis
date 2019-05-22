3c71838fb959d394664b58516247931f5d965929
Gaurav Roy <gauravroy0408@gmail.com>
2019-05-22 19:14:56 +0530

    Added all files

---
+++ pca_GR.py
@@ -0,0 +1,90 @@
+#####################################################################################
+# Creator     : Gaurav Roy
+# Date        : 22 May 2019
+# Description : The code performs PCA on Wine.csv to find the 2 best Independant 
+#               Variables to fit to Logistic Regression model.
+#####################################################################################
+
+# Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import Dataset
+dataset = pd.read_csv('Wine.csv')
+X = dataset.iloc[:,0:13].values
+Y = dataset.iloc[:,13].values
+
+# Splitting to Training and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state= 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Applying PCA
+from sklearn.decomposition import PCA
+
+#pca = PCA(n_components=None)
+#X_train = pca.fit_transform(X_train)
+#X_test = pca.fit_transform(X_test)
+#explained_variance = pca.explained_variance_ratio_
+
+# The first 2 IVs have variance total of about 56% of total variance which is good enough
+pca = PCA(n_components=2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+# Fitting Logistic Regression to Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test Set Results
+Y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Visualizing the training results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start= X_set[:,0].min()-1, stop= X_set[:,0].max()+1, step= 0.01),
+                     np.arange(start= X_set[:,1].min()-1, stop= X_set[:,1].max()+1, step= 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha= 0.6, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set==j, 0], X_set[Y_set==j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j, edgecolors='black')
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+
+# Visualizing the test results
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start= X_set[:,0].min()-1, stop= X_set[:,0].max()+1, step= 0.01),
+                     np.arange(start= X_set[:,1].min()-1, stop= X_set[:,1].max()+1, step= 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha= 0.6, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set==j, 0], X_set[Y_set==j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j, edgecolors='black')
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
